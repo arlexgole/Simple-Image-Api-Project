eb0faad4621c5a3a7a3f221ea3ba7cd3cef96eea
Arlex Gole <arlex97@gmail.com>
2020-04-22 23:39:11 -0400

    more copy changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -16,7 +16,7 @@ class SearchBar extends React.Component {
             <div className="ui segment"> 
                 <form className="ui form" onSubmit = {this.onFormSubmit}> 
                     <div className="field"> 
-                        <label> Search Image </label>
+                        <label> Search Images </label>
                         <input type="text" 
                             value = {this.state.term}
                             onChange={(e) => this.setState({ term: e.target.value})}
